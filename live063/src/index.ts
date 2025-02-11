b9a1920df28e107256580bb8d433ab0cbba0b6b3
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-02-10 23:24:55 -0300

    feat: create a route with types

--- live063/src/index.ts
+++ live063/src/index.ts
@@ -1,6 +1,34 @@
-import Fastify from 'fastify'
+import { randomUUID } from 'crypto';
+import Fastify, {type FastifyRequest} from 'fastify'
 
 const fastify = Fastify()
+
+type Request = FastifyRequest<{
+  Body: {name: string};
+  Headers: {org: string};
+  Params: {id: string};
+  Querystring: {page: string};
+  Reply: {
+    201: {
+      id: string
+    },
+    '4xx': {
+      message: string
+    }
+  }
+}>
+
+fastify.post('/teste/:id', async (request: Request, reply) => {
+  const {body, headers, params, query} = request
+
+  if(!body.name) {
+    return reply.code(400).send({message: 'Name is required.'})
+  }
+
+  return reply.code(201).send({
+    id: randomUUID()
+  })
+})
 
 async function main() {
   try {
